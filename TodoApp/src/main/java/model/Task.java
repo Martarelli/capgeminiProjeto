9c675ff138c6d64dec69eda612e2602020efe22d
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-05-12 19:32:07 -0300

    gerado classe Task

---
+++ TodoApp/src/main/java/model/Task.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Martarelli
+ */
+public class Task {
+    
+    private int id;
+    private int idProject;
+    private String name;
+    private String description;
+    private String notes;
+    private boolean isCompleted;
+    private Date deadline;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public Task(int id, int idProject, String name, String description, String notes, boolean isCompleted, Date deadline, Date createdAt, Date updatedAt) {
+        this.id = id;
+        this.idProject = idProject;
+        this.name = name;
+        this.description = description;
+        this.notes = notes;
+        this.isCompleted = isCompleted;
+        this.deadline = deadline;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+    
+    
+}

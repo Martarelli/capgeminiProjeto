3845440ab1f0a920e842e90aeaad29560e855003
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-05-12 21:55:49 -0300

    criação do metodo update

--- TodoApp/src/main/java/controller/ProjectController.java
+++ TodoApp/src/main/java/controller/ProjectController.java
@@ -51,7 +51,43 @@ public class ProjectController {
         } finally {
             ConnectionFactory.closeConnection(connection, statement);
         }
-        
+      
         
     }
+    
+    public void update(Project project){
+    
+        String sql = "UPDATE projects SET "
+                + "name= ?,"
+                + "description = ?,"
+                + "createdAt = ?,"
+                + "updateAt = ? "
+                + "WHERE id = ?";
+        
+        Connection connection = null;
+        PreparedStatement statement = null;
+    
+        try {
+            //CRIA CONEXÃO COM O BANCO DE DADOS
+            connection = ConnectionFactory.getConnection();
+            //CRIA UM PREPAREDSTATEMENT, CLASSE USADA PARA EXECUTAR A QUERY
+            statement = connection.prepareStatement(sql);
+            //SETANDO VALORES NO STATEMENT
+            statement.setString(1, project.getName());
+            statement.setString(2, project.getDescription());
+            statement.setDate(3, new Date(project.getCreatedAt().getTime()));
+            statement.setDate(4, new Date(project.getUpdatedAt().getTime()));
+            statement.setInt(5, project.getId());
+            //EXECUTANDO A QUERY
+            statement.execute();
+            
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao salvar o projeto" + 
+                    ex.getMessage(), ex);
+        } finally {
+            ConnectionFactory.closeConnection(connection, statement);
+        }
+    }
+    
+    
 }
